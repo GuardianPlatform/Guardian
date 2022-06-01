0f25698f178a6b3dcdcd9ebcbb7a2a1684144dc4
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-06-01 13:14:13 +0200

    add update-entities migration

--- Guardian.Backend/Guardian.Persistence/Migrations/Application/20220601103745_update-entities.Designer.cs
+++ Guardian.Backend/Guardian.Persistence/Migrations/Application/20220601103745_update-entities.Designer.cs
@@ -4,14 +4,16 @@ using Guardian.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Guardian.Persistence.Migrations.Application
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601103745_update-entities")]
+    partial class updateentities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
