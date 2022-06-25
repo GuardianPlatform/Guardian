97a0abb85d25df5afe198a9630ac2be5a7423e94
Kamil Cep <kamil.cep@microsoft.wsei.edu.pl>
2022-06-25 13:49:49 +0200

    Add commands to game controller

---
+++ Guardian.Backend/Guardian.Service/Features/CustomerFeatures/Commands/CreateGameCommand.cs
@@ -0,0 +1,39 @@
+﻿using Guardian.Domain.Entities;
+using Guardian.Persistence;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Guardian.Infrastructure.Database;
+
+namespace Guardian.Service.Features.CustomerFeatures.Commands
+{
+    class CreateGameCommand : IRequest<string>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string License { get; set; }
+
+        public class CreateGameCommandHandler : IRequestHandler<CreateGameCommand, string>
+        {
+            private readonly IApplicationDbContext _context;
+            public CreateGameCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<string> Handle(CreateGameCommand request, CancellationToken cancellationToken)
+            {
+                var game = new Game();
+                game.Name = request.Name;
+                game.Description = request.Description;
+                game.Author = request.Author;
+                game.License = request.License;
+
+                _context.Games.Add(game);
+                await _context.SaveChangesAsync();
+                return game.Name;
+            }
+        }
+    }
+    
+}
