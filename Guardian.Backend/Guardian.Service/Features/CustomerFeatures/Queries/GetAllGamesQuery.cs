a5cbfc418be439eb171c3de6d67bb3e81bc4d585
Kamil Cep <kamil.cep@microsoft.wsei.edu.pl>
2022-06-25 12:04:27 +0100

    Dodanie parametru

--- Guardian.Backend/Guardian.Service/Features/CustomerFeatures/Queries/GetAllGamesQuery.cs
+++ Guardian.Backend/Guardian.Service/Features/CustomerFeatures/Queries/GetAllGamesQuery.cs
@@ -6,36 +6,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Guardian.Domain.Entities;
+
 
 namespace Guardian.Service.Features.CustomerFeatures.Queries
 {
     public class GetAllGamesQuery : IRequest<IEnumerable<Game>>
-    {       
-        PagiantionParams pagiantionParams = new PagiantionParams();
+    {
+        public PagiantionParams Pagiantion { get; set; }
 
-            public class GetAllGamesQueryHandler : IRequestHandler<GetAllGamesQuery, IEnumerable<Game>>
+        public class GetAllGamesQueryHandler : IRequestHandler<GetAllGamesQuery, IEnumerable<Game>>
+        {
+
+            private readonly IApplicationDbContext _context;
+            public GetAllGamesQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<IEnumerable<Game>> Handle(GetAllGamesQuery request, CancellationToken cancellationToken)
             {
 
-                private readonly IApplicationDbContext _context;
-                public GetAllGamesQueryHandler(IApplicationDbContext context)
+                var gameList = await _context.Games
+                    .Skip(request.Pagiantion.ItemsPerPage * request.Pagiantion.page)
+                    .Take(request.Pagiantion.ItemsPerPage)
+                    .ToListAsync();
+                if (gameList == null)
                 {
-                    _context = context;
+                    return null;
                 }
-                public async Task<IEnumerable<Game>> Handle(GetAllGamesQuery request, CancellationToken cancellationToken)
-                {
-                    
-                    var gameList = await _context.Games
-                        .Skip(request.pagiantionParams.ItemsPerPage * request.pagiantionParams.page)
-                        .Take(request.pagiantionParams.ItemsPerPage)
-                        .ToListAsync();
-                    if (gameList == null)
-                    {
-                        return null;
-                    }
-                    return gameList.AsReadOnly();
-                }
+                return gameList.AsReadOnly();
             }
-        
+        }
+
     }
 }
