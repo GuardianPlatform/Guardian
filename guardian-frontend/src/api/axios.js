45b3cd13c36e8c600ed671db5a25b8b382dccec1
Michał Wojciechowski <kontogry96@gmail.com>
2022-07-08 15:47:41 +0200

    Add errorMessages in registration form

--- guardian-frontend/src/api/axios.js
+++ guardian-frontend/src/api/axios.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
-const BASE_URL = 'https://localhost:5001/api/';
+const BASE_URL = 'http://localhost:5001/api/';
 
-export default axios.create({
-    baseURL: BASE_URL
-});
-
-export const axiosPrivate = axios.create({
+const instance = axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    withCredentials: true,
 });
+
+instance.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    console.log(error);
+    if (typeof error.response.data.errors !== 'undefined') {
+        console.error(error.response.data.errors);
+    } else {
+        console.error(error.response.data.ErrorMessage)
+    }
+
+    return Promise.reject(error);
+});
+
+export default instance;
