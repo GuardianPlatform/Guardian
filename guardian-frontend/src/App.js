78f0683023771c8d850f31d455ff077b1e31d652
Michał Wojciechowski <kontogry96@gmail.com>
2022-07-10 12:09:24 +0200

    Add styles for categories

--- guardian-frontend/src/App.js
+++ guardian-frontend/src/App.js
@@ -8,15 +8,15 @@ import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
 
 function App() {
-  return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-        </Route>
-      </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
