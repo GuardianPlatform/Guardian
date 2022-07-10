fdb1bccc3dd9cbffe12959c36d7bedbab9ade918
Lordofblueflame <Cepkamil64@gmail.com>
2022-07-10 19:53:38 +0200

    PagiantionTest_SecondPageOneItem

--- Guardian.Backend/Guardian.Test.Unit/Persistence/GameControllerTests.cs
+++ Guardian.Backend/Guardian.Test.Unit/Persistence/GameControllerTests.cs
@@ -14,7 +14,7 @@ namespace Guardian.Test.Unit.Persistence
     {
         
         [Test]
-        public async Task Test()
+        public async Task PaginationTest_FirstPageThreeItems()
         {
                DbContextOptions<ApplicationDbContext> dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: "GameTests")
@@ -38,8 +38,31 @@ namespace Guardian.Test.Unit.Persistence
 
             Assert.AreEqual(MaxGamescount, result.Count());
         }
+        [Test]
+        public async Task PaginationTest_SecondPageOneItem()
+        {
+            DbContextOptions<ApplicationDbContext> dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+             .UseInMemoryDatabase(databaseName: "GameTests")
+             .Options;
+            var context = new ApplicationDbContext(dbContextOptions);
 
+            context.Add(new Game { Name = "gra1", Description = "dsc1", Author = "aa1" });
+            context.Add(new Game { Name = "gra2", Description = "dsc2", Author = "aa2" });
+            context.Add(new Game { Name = "gra3", Description = "dsc3", Author = "aa3" });
+            context.Add(new Game { Name = "gra4", Description = "dsc4", Author = "aa4" });
+            context.SaveChanges();
+            const int MaxGamescount = 3;
+            var request = new GetAllGamesQuery()
+            {
+                Pagination = new PagiantionModel(1, MaxGamescount)
+            };
 
+            var service = new GetAllGamesQuery.GetAllGamesQueryHandler(context);
+
+            var result = await service.Handle(request, default);
+
+            Assert.AreEqual(1, result.Count());
+        }
     }
 
 }
