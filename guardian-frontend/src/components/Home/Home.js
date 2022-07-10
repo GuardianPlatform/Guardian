b2b385c676b7ca16f1028bc708b796f14b17bcda
Michał Wojciechowski <kontogry96@gmail.com>
2022-07-10 12:08:55 +0200

    Add categories page

--- guardian-frontend/src/components/Home/Home.js
+++ guardian-frontend/src/components/Home/Home.js
@@ -60,9 +60,10 @@ const Home = () => {
                 console.log(response.data);
 
                 var mappedGames = Object.entries(response.data).map(([key, value]) =>
-                    <div className="game">
-                        <h3>{value.name}</h3>
-                        <p>{value.description}</p>
+                    <div className="game" onClick={selectGame}>
+                        <h4>{value.name}</h4>
+                        <img src={value.imageUrl} alt="Game Logo" className="image" />
+                        <div className="overlay">{value.description}</div>
                     </div>)
 
                 setGames(mappedGames);
@@ -87,44 +88,54 @@ const Home = () => {
         navigate('/login');
     }
 
+    const addGame = async () => {
+
+    }
+
+    const selectGame = async () => {
+        navigate('/game')
+    }
+
     return (
         <section className="home-wrapper">
 
-            {/*HEADER*/}
-            <header>
+            <div className="top">
                 <img src={require('../../assets/logo.png')}
                     className="logo"
                     alt="brand-logo" />
-                <div className="top">
-                    <div className="logout">
-                        <button onClick={logout} className="logout">Logout</button>
-                    </div>
-                    <div className="add-game">
-                        <button onClick={logout} className="add-game">Add game</button>
-                    </div>
-                </div>
-            </header>
-            {/* HEADER END*/}
-            <header className="categories-wrapper">
-                <ul className="category-list">
-                    {categories.map((category) =>
-                        <li
-                            key={category.categoryName}
-                            id={`category${category.id}`}
-                            className="category"
-                        >
-                            <a href={`#` + category.categoryName}
-                                onClick={(e) => { setCategory(category.categoryName) }}>
-                                {category.categoryName}
-                            </a>
+                <nav>
+                    <ol>
+                        <li>
+                            <button onClick={addGame} className="add-game">Add game</button>
                         </li>
-                    )}
-                </ul>
-            </header>
+                        <li>
+                            <button onClick={logout} className="logout">Logout</button>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+            <div className="content">
+                <header className="categories-wrapper">
+                    <ul className="category-list">
+                        {categories.map((category) =>
+                            <li
+                                key={category.categoryName}
+                                id={`category${category.id}`}
+                                className="category"
+                            >
+                                <a href={`#` + category.categoryName}
+                                    onClick={(e) => { setCategory(category.categoryName) }}>
+                                    {category.categoryName}
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </header>
 
-            <header className="games-wrapper">
-                {games.map((game) => game)}
-            </header>
+                <header className="games-wrapper">
+                    {games.map((game) => game)}
+                </header>
+            </div>
         </section >
 
 
