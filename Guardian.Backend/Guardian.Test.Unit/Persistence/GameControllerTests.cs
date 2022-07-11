15c4ba177ea308952702ed997f90847e6febd1ec
Lordofblueflame <Cepkamil64@gmail.com>
2022-07-11 14:33:58 +0200

    CreateGameQueryTest_ReturnId

--- Guardian.Backend/Guardian.Test.Unit/Persistence/GameControllerTests.cs
+++ Guardian.Backend/Guardian.Test.Unit/Persistence/GameControllerTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Guardian.Infrastructure.Database;
 using Guardian.Service.Features.Game.Queries;
 using System;
+using Guardian.Service.Features.Game.Commands;
 
 namespace Guardian.Test.Unit.Persistence
 {
@@ -137,6 +138,35 @@ namespace Guardian.Test.Unit.Persistence
             var result = await service.Handle(request, default);
 
             Assert.AreEqual(ListOfGames[2].Id, result.Id);
+        }
+
+        [Test]
+        public async Task CreateGameQueryTest_ReturnId()
+        {
+            DbContextOptions<ApplicationDbContext> dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+             .Options;
+            var context = new ApplicationDbContext(dbContextOptions);
+
+            context.Add(new Game { Name = "gra1", Description = "dsc1", Author = "aa1" });
+            context.Add(new Game { Name = "gra2", Description = "dsc2", Author = "aa2" });
+            context.Add(new Game { Name = "gra3", Description = "dsc3", Author = "aa3" });
+            context.Add(new Game { Name = "gra4", Description = "dsc4", Author = "aa4" }); //Id4 in list[3]
+            context.SaveChanges();
+
+            var request = new CreateGameCommand()
+            {
+                Name = "gra5",
+                Description = "desc5",
+                Author = "aa5",
+            };
+
+            var service = new CreateGameCommand.CreateGameCommandHandler(context);
+
+            var result = await service.Handle(request, default);
+            context.SaveChanges();
+            var ListOfGames = context.Games.ToList();
+            Assert.AreEqual(ListOfGames[ListOfGames.Count - 1].Id.ToString(), result);
         }
     }
 
