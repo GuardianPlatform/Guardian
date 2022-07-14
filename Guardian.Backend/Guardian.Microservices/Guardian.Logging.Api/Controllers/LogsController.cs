8a2689cb8df2e661321ee4dac6e65b501cc80881
Michał Wojciechowski <kontogry96@gmail.com>
2022-07-15 00:18:26 +0200

    Add async lock for log controller

--- Guardian.Backend/Guardian.Microservices/Guardian.Logging.Api/Controllers/LogsController.cs
+++ Guardian.Backend/Guardian.Microservices/Guardian.Logging.Api/Controllers/LogsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Guardian.Logging.Contract;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,23 @@ namespace Guardian.Logging.Api.Controllers
     [Route("api/[controller]")]
     public class LogsController : ControllerBase
     {
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Log log)
         {
             var message = $"[{log.LogLevel}] {log.DateTime:yyyy-MM-dd HH:mm:ss.fff} - {log.Message}" + Environment.NewLine;
-            await System.IO.File.AppendAllTextAsync($"{Environment.CurrentDirectory}\\log.txt", message);
+
+            await semaphore.WaitAsync();
+            try
+            {
+                await System.IO.File.AppendAllTextAsync($"{Environment.CurrentDirectory}\\log.txt", message);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+
             return Ok(log);
         }
     }
