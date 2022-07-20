a0825510e9e0db1b274e50d2558b94a522313f85
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-07-20 21:18:21 +0200

    Checking commit

---
+++ Guardian.Backend/Guardian.Test.Unit/CategoryTests/UpdateCategoryCommandHandlerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guardian.Domain.Entities;
+using Guardian.Service.Features.Category.Commands;
+using Guardian.Service.Features.Category.Queries;
+using Guardian.Service.Features.Game.Commands;
+using Guardian.Test.Unit.Persistence;
+using NUnit.Framework;
+using Org.BouncyCastle.Math.EC.Rfc7748;
+
+namespace Guardian.Test.Unit.CategoryTests
+{
+    public class UpdateCategoryCommandHandlerTests
+    {
+
+        [Test]
+        public async Task UpdateCategoryTest_IsCategoryUpdated()
+        {
+            var context = TestDbContext.TestDbContextMethod(out var dbContextOptions);
+
+            context.Add(new Category {CategoryName = "Kategoria", Id = 0});
+
+            await context.SaveChangesAsync();
+
+            
+
+            var request = new UpdateCategoryCommand()
+            {
+                CategoryName = "Nowa Kategoria",
+                Id = 0
+            };
+
+            var category = context.Categories.FirstOrDefault(c => c.CategoryName == "Kategoria");
+
+            category.CategoryName = request.CategoryName;
+
+            context.Update(category);
+
+            await context.SaveChangesAsync();
+
+            var categoriesQuery = new GetAllCategoriesQuery();
+
+            var categoriesQueryService = new GetAllCategoriesQuery.GetAllCategoriesQueryHandler(context);
+
+
+            string defaultName = "Nowa Kategoria";
+
+            var result = await categoriesQueryService.Handle(categoriesQuery, default);
+            
+
+            Assert.AreEqual(defaultName, result.FirstOrDefault().CategoryName);
+        }
+    }
+}
