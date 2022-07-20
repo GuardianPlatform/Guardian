cccdd45e1bbed5af9c84b4f9f40c95a309636082
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-07-20 12:21:28 +0200

    Add categories controller test

---
+++ Guardian.Backend/Guardian.Test.Unit/Persistence/CategoriesControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guardian.Domain.Entities;
+using Guardian.Infrastructure.Database;
+using Guardian.Service.Features.Category.Queries;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Org.BouncyCastle.Math.EC.Rfc7748;
+
+namespace Guardian.Test.Unit.Persistence
+{
+        
+    public class CategoriesControllerTests
+    {
+
+        [Test]
+        public async Task GetAllCategoriesTest_ReturnCorrectNumberOfCategory()
+        {
+            
+            var context = TestDbContext.TestDbContextMethod(out var dbContextOptions);
+
+            context.Add(new Category {CategoryName = "Kategoria 1"});
+            context.Add(new Category {CategoryName = "Kategoria 2"});
+            context.Add(new Category {CategoryName = "Kategoria 3"});
+            context.SaveChanges();
+
+            const int numberOfGames = 3;
+
+            var request = new GetAllCategoriesQuery() { };
+
+            var service = new GetAllCategoriesQuery.GetAllCategoriesQueryHandler(context);
+
+            var result = await service.Handle(request, default);
+
+            Assert.AreEqual(numberOfGames, result.Count());
+        }
+
+        [Test]
+        public async Task GetAllCategoriesTest_EmptyCategoryList()
+        {
+            var context = TestDbContext.TestDbContextMethod(out var dbContextOptions);
+
+            const int numberOfGames = 0;
+            
+            var request = new GetAllCategoriesQuery();
+
+            var service = new GetAllCategoriesQuery.GetAllCategoriesQueryHandler(context);
+
+            var result = await service.Handle(request, default);
+
+            Assert.AreEqual(numberOfGames, result.Count());
+        }
+
+        [Test]
+        public async Task 
+    }
+}
---
+++ Guardian.Backend/Guardian.Test.Unit/Persistence/TestDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guardian.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guardian.Test.Unit.Persistence
+{
+    public class TestDbContext
+    {
+        public static ApplicationDbContext TestDbContextMethod(out DbContextOptions<ApplicationDbContext> dbContextOptions)
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ApplicationDbContext(dbContextOptions);
+            return context;
+        }
+    }
+}
